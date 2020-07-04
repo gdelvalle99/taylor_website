2cc5baab2157b7df58a2d07c52fe35217c2e17db
Guillermo Del Valle <gdelvalle@nevada.unr.edu>
2020-07-04 00:53:19 -0700

    mailchimp added

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
       {
         resolve: 'gatsby-plugin-mailchimp',
         options: {
-            endpoint: '', // string; add your MC list endpoint here; see instructions below
+            endpoint: 'https://gmail.us10.list-manage.com/subscribe/post?u=1f844c287b5f30899127e2566&amp;id=64e7a569e3', // string; add your MC list endpoint here; see instructions below
             timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
         },
       },
