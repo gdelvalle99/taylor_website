d202e097e387405487b8651824df98ad594be722
Guillermo Del Valle <gdelvalle@nevada.unr.edu>
2020-07-14 11:19:44 -0700

    styled the gallery a bit

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Menu from "../components/menu"
+import ImageComponent from "../components/ImageComponent"
 import globalStyle from "./global.module.css"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
@@ -8,7 +9,7 @@ import Img from "gatsby-image"
 const imgGridStyle = {
   display: 'grid',
   gridTemplateColumns:`repeat(auto-fill, 200px)`,
-  margin: '10px'
+  margin: '10px',
 };
 
 export default function Contact({ data }) {
@@ -16,7 +17,9 @@ export default function Contact({ data }) {
   <Menu />
   <div style={imgGridStyle}>
   {data.allFile.edges.map(edge =>
+    <div>
     <Img className={globalStyle.myImg} fluid={edge.node.childImageSharp.fluid} alt=""/>
+    </div>
   )}
   </div>
   </div>
